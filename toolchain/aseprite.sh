77e92cc780eb2a5fbdfa40c70649ab78d3fd4220
Anson Mansfield <anson.mansfield@gmail.com>
2023-04-24 12:15:51 -0400

    try to be smarter about it

--- toolchain/aseprite.sh
+++ toolchain/aseprite.sh
@@ -1,4 +1,39 @@
 #!/bin/sh
 
-echo $@
-'/c/Program Files (x86)/Steam/steamapps/common/Aseprite/Aseprite.exe' $@
+asepriteId=431730
+
+uname="$(uname -s)"
+case "${uname}" in
+    Linux*)
+        steamPath='~/.local/share/Steam'
+        steamBin='steam'
+        asepriteBin="${steamPath}/steamapps/common/Aseprite/Aseprite"
+        ;;
+    Darwin*) 
+        steamPath='~/.local/share/Steam'
+        steamBin='steam'
+        asepriteBin="${steamPath}/steamapps/common/Aseprite/Aseprite"
+        ;;
+    CYGWIN*)
+        steamPath='/c/Program Files (x86)/Steam'
+        steamBin="${steamPath}/steam.exe"
+        asepriteBin="${steamPath}/steamapps/common/Aseprite/Aseprite.exe"
+        ;;
+    MINGW*)
+        steamPath='/c/Program Files (x86)/Steam'
+        steamBin="${steamPath}/steam.exe"
+        asepriteBin="${steamPath}/steamapps/common/Aseprite/Aseprite.exe"
+        ;;
+    *)
+        echo "unknown platform ${uname}"
+        exit 1
+esac
+
+if [ -f "${asepriteBin}" ] ;
+then
+    set -x
+    "${asepriteBin}" $@
+else
+    set -x
+    "${steamBin}" -applaunch $asepriteId "$@"
+fi
